3fcae39d992c41cffa8e351b98c43ed2839da153
Nikilesh Raj <nikileshnatraj@gmail.com>
2024-07-05 00:37:30 +0530

    added single state

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,29 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer((currActivePlayer) => {
-      return currActivePlayer === "X" ? "O" : "X";
-    });
-
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentActivePlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+        {
+          square: { row: rowIndex, col: colIndex },
+          player: currentActivePlayer,
+        },
         ...prevTurns,
       ];
 
@@ -36,13 +41,10 @@ function App() {
           <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
 
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
 
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
